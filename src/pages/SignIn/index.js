35ed4008c9fb786862577d1577c3d91418d31fe2
Flavio Martins da Silva <flavio.martins@ormecmvr.net>
2019-08-26 15:08:38 -0300

    Página do SignIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import {Image} from 'react-native';
 import Background from '~/components/Background';
-import Input from '~/components/Input';
-import Button from '~/components/Button';
-import {Container} from './styles';
+import logo from '~/assets/logo.png';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Input
-        icon="call"
-        placeholder="Digite seu nome"
-        style={{marginTop: 30}}
-      />
-      <Button>Entrar</Button>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
