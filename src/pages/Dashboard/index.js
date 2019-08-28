f6e5aa7c5e70639f28d789844efb11ae9d9be21a
Flavio Martins da Silva <flavio.martins@ormecmvr.net>
2019-08-28 15:34:43 -0300

    Listagem de agendamentos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import Background from '~/components/Background';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import {Container, Title, List} from './styles';
+import Appointment from '~/components/Appointment';
 
+const data = [1, 2, 3, 4, 5];
 export default function Dashboard() {
-  return <Background />;
+  return (
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+        <List
+          data={data}
+          keyExtractor={item => String(item)}
+          renderItem={({item}) => <Appointment data={item} />}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 Dashboard.navigationOptions = {
