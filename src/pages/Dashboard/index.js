163d1abb18371a338acc9b4cfe8d858d73ef28fa
Flavio Martins da Silva <flavio.martins@ormecmvr.net>
2019-08-28 16:41:09 -0300

    Agendamento da API

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Background from '~/components/Background';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {Container, Title, List} from './styles';
 import Appointment from '~/components/Appointment';
+import api from '~/services/api';
 
-const data = [1, 2, 3, 4, 5];
 export default function Dashboard() {
+  const [appointments, setAppointments] = useState([]);
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get('appointment');
+      setAppointments(response.data);
+    }
+    loadAppointments();
+  }, []);
+
+  async function handleCancel(id) {
+    const response = await api.delete(`appointment/${id}`);
+    setAppointments(
+      appointments.map(appointment =>
+        appointment.id === id
+          ? {
+              ...appointment,
+              canceled_at: response.data.canceled_at,
+            }
+          : appointment,
+      ),
+    );
+  }
   return (
     <Background>
       <Container>
         <Title>Agendamentos</Title>
         <List
-          data={data}
-          keyExtractor={item => String(item)}
-          renderItem={({item}) => <Appointment data={item} />}
+          data={appointments}
+          keyExtractor={item => String(item.id)}
+          renderItem={({item}) => (
+            <Appointment onCancel={() => handleCancel(item.id)} data={item} />
+          )}
         />
       </Container>
     </Background>
